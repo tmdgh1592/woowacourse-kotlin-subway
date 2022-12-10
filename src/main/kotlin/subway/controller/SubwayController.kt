35cd16c49a945d07d39c58d6d7ce4edea72b9daf
tmdgh1592 <tmdgh1592@naver.com>
2022-12-10 23:02:08 +0900

    feat: 노선별 역 초기 설정

--- src/main/kotlin/subway/controller/SubwayController.kt
+++ src/main/kotlin/subway/controller/SubwayController.kt
@@ -1,17 +1,25 @@
 package subway.controller
 
+import camp.nextstep.edu.missionutils.Console
 import subway.presentation.InputView
 import subway.presentation.OutputView
+import subway.service.LineService
+import subway.service.StationService
 
 class SubwayController(
     inputView: InputView, outputView: OutputView
 ) : Controller(inputView, outputView) {
+    private val stationService = StationService()
+    private val lineService = LineService()
+
+    init {
+        lineService.init()
+    }
 
     override fun run() {
         do {
             val mainOption = selectMainOption()
         } while (mainOption != QUIT_OPTION)
-
     }
 
     private fun selectMainOption(): String {
--- src/main/kotlin/subway/service/LineService.kt
+++ src/main/kotlin/subway/service/LineService.kt
@@ -3,8 +3,22 @@ package subway.service
 import subway.repository.LineRepository
 
 class LineService {
+    fun init() {
+        addStation(1, "2호선", "교대역")
+        addStation(2, "2호선", "강남역")
+        addStation(3, "2호선", "역삼역")
 
-    fun addStationOf(sequence: Int, lineName: String, stationName: String) {
+        addStation(1, "3호선", "교대역")
+        addStation(2, "3호선", "남부터미널역")
+        addStation(3, "3호선", "양재역")
+        addStation(4, "3호선", "매봉역")
+
+        addStation(1, "신분당선", "강남역")
+        addStation(2, "신분당선", "양재역")
+        addStation(3, "신분당선", "양재시민의숲역")
+    }
+
+    fun addStation(sequence: Int, lineName: String, stationName: String) {
         val line = LineRepository.getLine(lineName)
         line.addStation(sequence, stationName)
     }
